6d04217cbeb4b63dd0f398ed07e7f18fc0142441
merdjiMarwan <144659633+merdjiMarwan@users.noreply.github.com>
2024-11-18 16:45:19 +0000

    multiplication

--- code/multiplication.php
+++ code/multiplication.php
@@ -2,24 +2,22 @@
 
 $max = 5;
 
-echo "Table de multiplication jusqu'à $max x $max\br";
-
+echo "Table de multiplication jusqu'à $max x $max<br>";
 
 echo str_pad(" ", 5); 
 for ($i = 1; $i <= $max; $i++) {
     echo str_pad($i, 5, " ", STR_PAD_BOTH); 
-echo "\br";
+}
+echo "<br>";
 
-
-echo str_repeat("-", $max * 5 + 5) . "\br";
-
+echo str_repeat(" ", $max * 5 + 5) . "<br>";
 
 for ($i = 1; $i <= $max; $i++) {
     echo str_pad($i, 5, " ", STR_PAD_BOTH); 
     for ($j = 1; $j <= $max; $j++) {
         echo str_pad($i * $j, 5, " ", STR_PAD_BOTH); 
     }
-    echo "\br";
+    echo "<br>";
 }
-}
+
 ?>
